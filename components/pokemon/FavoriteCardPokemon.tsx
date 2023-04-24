6d858bacfc87ef8e24d279abe201b90585351022
Diego Oliver <diegooliver.globant@latam.com>
2023-04-23 22:01:13 -0400

    resolved errors

--- components/pokemon/FavoriteCardPokemon.tsx
+++ components/pokemon/FavoriteCardPokemon.tsx
@@ -1,28 +1,33 @@
-import { Card, Grid } from "@nextui-org/react";
-import { useRouter } from "next/router";
-import { FC } from "react";
+import { FC } from 'react';
+import { useRouter } from 'next/router';
+
+import { Grid, Card } from '@nextui-org/react';
+
 
 interface Props {
     pokemonId: number;
 }
 
-export const FavoriteCardPokemon: FC<Props> = ({ pokemonId }) => {
 
-    const router = useRouter();
+export const FavoriteCardPokemon:FC<Props> = ({ pokemonId }) => {
 
-    const onFavoriteClicked = () => {
-        router.push(`/pokemon/${pokemonId}`);
-    }
+  const router = useRouter();
 
-    return (
-        <Grid xs={6} sm={3} md={2} xl={1} key={pokemonId} >
-            <Card isHoverable isPressable css={{ padding: 10 }} onPress={onFavoriteClicked}>
-                <Card.Image
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`}
-                    width={'100%'}
-                    height={140}
-                />
-            </Card>
-        </Grid>
-    )
-}
+
+  const onFavoriteClicked = () => {
+    router.push(`/pokemon/${ pokemonId }`);
+  }
+
+
+  return (
+    <Grid xs={ 6 } sm={ 3 } md={ 2 } xl={ 1 } key={ pokemonId } onClick={ onFavoriteClicked }>
+        <Card isHoverable isPressable css={{ padding: 10 }} key={ pokemonId }>
+        <Card.Image 
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ pokemonId }.svg`}
+            width={'100%'}
+            height={ 140 }
+        />
+        </Card>
+    </Grid>
+  )
+};
